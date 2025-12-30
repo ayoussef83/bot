ace9bc617fb73593913a9049ef4999052c02e501
ayoussef83 <eng.a.m.youssef@gmail.com>
2025-12-30 03:18:35 +0200

    fix: export SettingsService for scheduler module

--- Mvalley System/backend/src/settings/settings.module.ts
+++ Mvalley System/backend/src/settings/settings.module.ts
@@ -7,6 +7,7 @@ import { SettingsService } from './settings.service';
   imports: [PrismaModule],
   controllers: [SettingsController],
   providers: [SettingsService],
+  exports: [SettingsService],
 })
 export class SettingsModule {}
 
